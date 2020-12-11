a6023e46f9c37bf3169772163311ef3bced2f636
Lydie <lydie_debaize@dfci.harvard.edu>
2020-12-11 11:06:17 -0500

    Added information to readme

--- code/scriptlets.R
+++ code/scriptlets.R
@@ -1,13 +1,44 @@
 #!/usr/bin/env Rscript
 
-# Put comments here
-#  including pseudo-code of program flow
+#!/usr/bin/env Rscript
 
-# Put globals, installs, and sources here
+# This script will include a collection of small scripts steps
+# often seen as example code. We're using this solely for demo purposes
 
-# Put functions here
 
-# now your main code
+# 1. Prints hello world
+
+myString <- "Hello, World!"
+
+print (myString)
+
+
+# 2. Square function
+# adapted from https://hbctraining.github.io/Intro-to-R/lessons/03_introR-functions-and-arguments.html#user-defined-functions
+# and https://www.r-bloggers.com/how-to-write-and-debug-an-r-function/
+
+square_it <- function(x){
+  sq <- x*x
+  return(sq)
+}
+
+square_it(5)
+
+
+# 3. Monte Carlo Pi
+
+for (trials in 1:3000) {
+  count = 0
+  for(i in 1:trials) {
+    if((runif(1,0,1)^2 + runif(1,0,1)^2) < 1) {
+      count = count + 1
+    }
+  }
+  print(paste(trials, ": ", (count*4) / trials))
+}
+
+
+# END
 
 # END
 
